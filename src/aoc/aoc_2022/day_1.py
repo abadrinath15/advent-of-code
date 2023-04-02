b1510291418f0ba9f6f6383f51a5587ed22bd297
Aditya Badrinath <abadrinath15@gmail.com>
2023-04-01 20:16:54 -0400

    day 1

--- src/aoc/aoc_2022/day_1.py
+++ src/aoc/aoc_2022/day_1.py
@@ -1,10 +1,28 @@
+from typing import Iterable
 from itertools import groupby
+import heapq
+
+
+def total_elf_calories(elf_snacks: Iterable[str]) -> int:
+    return sum(map(lambda x: int(x) if x != "" else 0, elf_snacks))
 
 
 def most_calories(input_fp: str) -> int:
     curr_max = 0
     with open(input_fp) as file:
-        for _, g in groupby((line.strip("\n") for line in file), lambda x: x == ""):
-            curr_max = max(curr_max, sum(map(lambda x: int(x) if x != "" else 0, g)))
+        for k, es in groupby((line.strip("\n") for line in file), lambda x: x != ""):
+            if k:
+                curr_max = max(curr_max, total_elf_calories(es))
 
     return curr_max
+
+
+def top_three_calories(input_fp: str) -> int:
+    top_cals: list[int] = []
+    with open(input_fp) as file:
+        grps = groupby((line.strip("\n") for line in file), lambda x: x != "")
+        for k, es in grps:
+            if k:
+                heapq.heappush(top_cals, total_elf_calories(es))
+
+    return sum(heapq.nlargest(3, top_cals))
--- tests/aoc_2022/test_day_1.py
+++ tests/aoc_2022/test_day_1.py
@@ -3,3 +3,7 @@ from aoc.aoc_2022 import day_1
 
 def test_most_calories() -> None:
     assert day_1.most_calories("src/aoc/aoc_2022/inputs/day_1_test.txt") == 24000
+
+
+def test_top_three_calories() -> None:
+    assert day_1.top_three_calories("src/aoc/aoc_2022/inputs/day_1_test.txt") == 45000
