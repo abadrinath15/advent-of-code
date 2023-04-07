e5b138c348cbe1e58f9168cfa1ab07fefa0ba15d
Aditya Badrinath <abadrinath15@gmail.com>
2023-04-07 00:15:07 -0400

    yikes that sucked!

--- src/aoc/aoc_2022/day_5.py
+++ src/aoc/aoc_2022/day_5.py
@@ -1,3 +1,20 @@
+from collections import deque
+import re
+
+
+def row_to_instruction(row: str, stacks: list[deque[str]]) -> None:
+    matches = re.match(r"move (\d+) from (\d+) to (\d+)", row)
+    if matches is None:
+        raise ValueError
+
+    how_many, from_s, to_s = (int(matches[x]) for x in range(1, 4))
+    from_s -= 1
+    to_s -= 1
+    while how_many > 0:
+        stacks[to_s].append(stacks[from_s].pop())
+        how_many -= 1
+
+
 def row_to_cols(row: str) -> list[int]:
     return [int(num) for num in row.split()]
 
@@ -19,3 +36,32 @@ def row_to_stack(row: str) -> list[str | None]:
             res.append(None)
 
         row = row[1:]
+
+
+def main(input_fp: str) -> str:
+    temp_stacks: list[list[str | None]] = []
+    with open(input_fp) as file:
+        file_iter = iter(file)
+        for line in file_iter:
+            if line[1] == "1":
+                break
+
+            temp_stacks.append(row_to_stack(line))
+
+        num_stacks = len(row_to_cols(line))
+        stacks: list[deque[str]] = [deque() for _ in range(num_stacks)]
+        for row in temp_stacks:
+            for col, stack in zip(row, stacks):
+                if col is not None:
+                    stack.appendleft(col)
+
+        next(file_iter)
+
+        for line in file_iter:
+            row_to_instruction(line, stacks)
+
+        return "".join(x.pop() for x in stacks)
+
+
+if __name__ == "__main__":
+    print(main("src/aoc/aoc_2022/inputs/day_5.txt"))
--- tests/aoc_2022/test_day_5.py
+++ tests/aoc_2022/test_day_5.py
@@ -1,4 +1,11 @@
 from aoc.aoc_2022 import day_5
+from collections import deque
+
+
+def test_row_to_instruction() -> None:
+    stacks = [deque(["Z", "N"]), deque(["M", "C", "D"]), deque(["P"])]
+    day_5.row_to_instruction("move 1 from 2 to 1", stacks)
+    assert stacks == [deque(["Z", "N", "D"]), deque(["M", "C"]), deque(["P"])]
 
 
 def test_row_to_cols() -> None:
@@ -9,3 +16,7 @@ def test_row_to_cols() -> None:
 def test_row_to_stack() -> None:
     assert day_5.row_to_stack("    [D]    \n") == [None, "D", None]
     assert day_5.row_to_stack("[N] [C]    \n") == ["N", "C", None]
+
+
+def test_main() -> None:
+    assert day_5.main("src/aoc/aoc_2022/inputs/day_5_test.txt") == "CMZ"
